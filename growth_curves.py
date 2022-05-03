c7e5de553402a3ae6b529d1d0e636b584ad33b07
Vladimir Gritsenko <vladimir.gritsenko@gmail.com>
2022-05-03 17:43:16 +0300

    Added documentation to the main functions in Numpydoc style.

--- growth_curves.py
+++ growth_curves.py
@@ -5,6 +5,66 @@ from openpyxl import load_workbook
 from itertools import product
 
 def read_plate_key(in_file, sheet_name="Keys", converters=None):
+    """Read the plate keys from a given sheet in an Excel file.
+    
+    The key sheet should consist of one or more **tables** separated by at
+    least one empty row, followed by an uppercase "STOP" (also separated by at
+    least one empty row). Rows after the "STOP" are ignored. Everything should
+    be aligned to the first column.
+    
+    The **table** format is as follows:
+    The first cell of the first row should store the name of the key, e.g.
+    "Strain" or "Media". The next row should be a 8-by-12 grid, with every cell
+    storing the key of the corresponding well (where the top-left cell
+    corresponds to the A1 well). No empty rows are allowed between the key name
+    and the key grid.
+    
+    An example with a 2x3 layout (instead of 8x12):
+    
+    +------+---+---+
+    |Strain|   |   |
+    +------+---+---+
+    |   1  | 2 | 3 |
+    +------+---+---+
+    |   1  | 2 | 3 |
+    +------+---+---+
+    |      |   |   |
+    +------+---+---+
+    | Media|   |   |
+    +------+---+---+
+    | YPD  |YPD|YPD|
+    +------+---+---+
+    | SDC  |SDC|SDC|
+    +------+---+---+
+    |      |   |   |
+    +------+---+---+
+    | STOP |   |   |
+    +------+---+---+
+    
+    Parameters
+    ----------
+    in_file : str
+        The path to the Excel file holding the keys.
+    sheet_name : str, default: ``"Keys"``
+        The sheet name of the sheet that holds the keys.
+    converters : dict of str to callable, optional
+        A dictionary of converters for the key values. The dict keys are key
+        names, while the dict values are callables that take the string value
+        from the sheet and convert it to the correct value for further
+        processing in Python.
+    
+    Returns
+    -------
+    keys : list
+        The keys parsed in the key sheet (list of strings). In the above example
+        this would be ``('Strain', 'Media')``.
+    key_values : dict of str to tuple
+        A dictionary mapping well names to a tuple of
+        key values (with the same length as the lengths of the returned `keys`).
+        In the above example this would be ``{'A1': (1, 'YPD'), 'A2': (2, 'YPD'),
+        'A3': (3, 'YPD'), 'B1': (1, 'SDC'), 'B2': (2, 'SDC'), 'B3': (3, 'SDC')}``.
+    """
+    
     rows = list("ABCDEFGH")
     cols = list(range(1, 13))
     
@@ -52,6 +112,32 @@ def read_plate_key(in_file, sheet_name="Keys", converters=None):
     return keys, result
 
 def read_experiment(in_file, sheet_name, key_sheet_name="Keys", converters=None):
+    """Parse plate growth curve data into a DataFrame.
+    
+    Reads the plate data along with the relevant keys (expected to be in the
+    same workbook). The DataFrame will have a MultiIndex over the rows as
+    parsed from the `key_sheet_name` sheet, with the last level called "Well"
+    and storing the well information. The columns are "Time (s)", "OD" and
+    "Temp" (temperature).
+    
+    Parameters
+    ----------
+    in_file : str
+        The path to the Excel input file.
+    sheet_name : str
+        The name of the sheet holding the plate data.
+    key_sheet_name : str, default: ``"Keys"``
+        The name of the sheet holding the key definitions.
+    converters : dict, optional
+        Converters for the key values, see the same parameter in the
+        `read_plate_key` function.
+    
+    Returns
+    -------
+    pandas.DataFrame
+        The parsed OD measurements for the plate.
+    """
+    
     index_names, cell_indexes = read_plate_key(in_file, key_sheet_name, converters)
     
     wb = load_workbook(filename=in_file)
@@ -94,27 +180,81 @@ def read_experiment(in_file, sheet_name, key_sheet_name="Keys", converters=None)
     return new_df
 
 def plot_ods(
-    df, x_index=None, y_index=None, x_title=None, y_title=None, cmap=None, style_func=None,
-    mean_indices=None, # A list of indices to group by for averaging (the other indices will be averaged over).
+    df,
+    x_index=None, y_index=None, x_title=None, y_title=None,
+    mean_indices=None,
     std_dev=None, # None, "bar", "area" -- must be used with mean_indices!
-    dpi=150,
+    style_func=None,
     legend=True,
     title_all_axes=False,
+    cmap="viridis",
+    dpi=150,
     alpha=1,
 ):
+    """Plot the ODs from a DataFrame returned by `read_experiment`.
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame with the growth curves to plot.
+    x_index : str, optional
+        The name of the level in `df`'s index to plot along the X axis
+        (columns) of the figure. The values of this level will be used as the
+        titles for the first row of Axes in the figure.
+    y_index : str, optional
+        The name of the level in `df`'s index to plot along the Y axis (rows)
+        of the figure. The values of this level will be used as the title of
+        the y axis for the first column of the Axes in the figure.
+    x_title : str, optional
+        If x_index is not specified, this will be used as the title of the
+        first Axes in the figure.
+    y_title: str, optional
+        If y_index is not specified, this will be used as the title of the y
+        axis of the first Axes in the figure.
+    mean_indices : sequence of str, optional
+        The index levels which will be used to group the growth curves for
+        averaging. For example, if ``("Strain", "Media")`` is passed, every unique
+        strain-media combination will be grouped, and all remaining sub-levels
+        will be averaged over to yield a single growth curve, which will then
+        be plotted. If different experiments within the group have different time stamps,
+        the "missing" measurements will be interpolated before averaging.
+    std_dev : {None, 'bar', 'area'}
+        If `mean_indices` is specified, this will set the method for displaying
+        the standard deviation:
+            
+        - **bar** - show the standard deviation as whiskers.
+        - **area** - show the standard deviation as a semi-transparent area.
+    style_func : callable, optional
+        A callable that accepts a tuple of ``(x_label, y_label, ix)`` where ix is
+        the index of the curve to draw. It should return a dict of kwargs for
+        the `Axes.plot` method.
+    legend : bool, default=True
+        If True, will add a legend of the curves at the end of each row.
+    title_all_axes : bool, default=False
+        If True, will set the title for all Axes (not just the top row).
+    cmap : str or `matplotlib.colors.Colormap`, default='viridis'
+        The colormap to use for the growth curves.
+    
+    Returns
+    -------
+    matplotlib.figure.Figure
+        The figure with the plots.
+    """
+    
     if std_dev is not None and mean_indices is None:
         raise ValueError("std_dev is not None but mean_indices was not given!")
     
-    if cmap is None:
-        cmap = cm.get_cmap("viridis")
-    elif isinstance(cmap, str):
+    if isinstance(cmap, str):
         cmap = cm.get_cmap(cmap)
     
     if mean_indices:
         df = avg_over_ixs(df, mean_indices)
     else:
+        # Work on a copy of the df, in case we'll need to modify it:
         df = df.copy()
     
+    # If the x/y indices are not specified, create singleton "dummy" levels to
+    # allow for the subequent code to be agnostic of these effects.
     if x_index is None:
         x_index = "_dummy_x"
         df[x_index] = ""
@@ -123,6 +263,9 @@ def plot_ods(
         y_index = "_dummy_y"
         df[y_index] = ""
         df.set_index(y_index, append=True, drop=True, inplace=True)
+        
+    # The code expects at least one level beyond the x/y levels, and if it
+    # doesn't exist, create it as we did in case the x/y levels.
     if len(df.index.levels) == 2:
         z_index = "_dummy_z"
         df[z_index] = ""
@@ -231,6 +374,32 @@ def plot_ods(
     return fig
 
 def avg_over_ixs(df_in, avg_levels, x_col="Time (s)", y_col="OD", interpolation_method="from_derivatives"):
+    """Return a DataFrame that averages all levels over a given set of levels.
+    
+    If the timestamps differ between any measurement series, measurements will
+    be interpolated for the missing timestamps.
+    
+    The new DataFrame will have `avg_levels` as the row MultiIndex and three
+    columns: `x_col`, `y_col mean` and `y_col std` (for standard deviation).
+    
+    Parameters
+    ----------
+    df_in : pandas.DataFrame
+        The input DataFrame.
+    avg_levels : sequence of str
+        The index levels to group by for averaging purposes.
+    x_col : str, default: "Time (s)"
+        The independent variable column name.
+    y_col : str, default: "OD"
+        The dependent variable column name.
+    interpolation_method : str, default: "from_derivatives"
+        The interpolation method.
+    
+    Returns
+    -------
+    pandas.DataFrame
+        A new DataFrame, with `avg_levels` as the new MultiIndex, and three 
+    """
     dfs_to_concat = []
     for exp_ix in df_in.index.droplevel([i for i in df_in.index.names if i not in avg_levels]).unique():
         exp_df = df_in.xs(exp_ix, level=avg_levels)[[x_col, y_col]]
